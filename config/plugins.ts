9ff8a587a3af1bb81ad1aabf3db749821db1a87c
ramonlnas <lopessramon0@gmail.com>
2024-08-02 18:02:01 -0300

    feat: initial config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,21 @@
-export default () => ({
+export default ({ env }) => ({
   "users-permissions": {
     config: {
       register: {
         allowedFields: ["cpf", "fullname"],
+      },
+    },
+  },
+
+  email: {
+    config: {
+      provider: "strapi-provider-email-brevo",
+      providerOptions: {
+        apiKey: env('BREVO_API_KEY'),
+      },
+      settings: {
+        // defaultSenderEmail: "noreply@daevg.com.br",
+        defaultReplyTo: "no-reply@varzeagrande.mt.gov.br",
       },
     },
   },
