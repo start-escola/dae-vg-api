6e96b1ae931268cdbb67edbae6e31ad0a7f3bb5b
ramonlnas <lopessramon0@gmail.com>
2024-08-07 16:51:13 -0300

    fix: syntax in plugin.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -10,15 +10,16 @@ export default ({ env }) => ({
     config: {
       provider: "strapi-provider-email-brevo",
       providerOptions: {
-        apiKey: env('BREVO_API_KEY'),
+        apiKey: env("BREVO_API_KEY"),
       },
       settings: {
         // defaultSenderEmail: "noreply@daevg.com.br",
         defaultReplyTo: "no-reply@varzeagrande.mt.gov.br",
       },
     },
-  ckeditor: {
-    enabled: true,
-    resolve: "./src/plugins/strapi-plugin-ckeditor",
+    ckeditor: {
+      enabled: true,
+      resolve: "./src/plugins/strapi-plugin-ckeditor",
+    },
   },
 });
